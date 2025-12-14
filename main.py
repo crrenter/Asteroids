a4ea695371753ea820d490f6d36be0415bb740e1
crrenter <crrenter@users.noreply.github.com>
2025-12-13 23:26:32 -0800

    shot asteroid collision

--- main.py
+++ main.py
@@ -45,6 +45,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
 
         pygame.display.flip()
         dt = clock.tick(60)/1000
